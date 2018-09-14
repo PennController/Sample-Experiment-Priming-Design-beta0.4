440e203ed50b96944b60ed72cf3315511a218bde
PennController <39499242+PennController@users.noreply.github.com>
2018-09-13 23:22:12 -0400

    priming design

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,13 +1,35 @@
 var shuffleSequence = seq("trial");
 PennController.ResetPrefix(null);
+PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
 
 var items = [
+    // Picture selection trial
     ["trial", "PennController", PennController(
-        newText("green", "How warm is the color green to you?")
+        defaultImage
+            .settings.size(200, 200)
+        ,
+        newText("test sentence", "Which patch is greener?")
             .print()
         ,
-        newScale("judgment", 5) // 5-point scale
+        newCanvas("patches", 500, 200)
+            .settings.add(   0, 0, newImage("color1", "green1.png") )   // Embedded image creation
+            .settings.add( 300, 0, newImage("color2", "green2.png") )   // Embedded image creation
             .print()
-            .wait()             // Validate upon choice
+        ,
+        newSelector("patch")
+            .settings.add( getImage("color1") , getImage("color2") )
+            .wait()
+    )]
+    ,
+    // Rating trial
+    ["trial", "PennController", PennController(
+        newText("green", "To me the color green is...")
+        ,
+        newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
+            .settings.labels("top")
+            .settings.before( getText("green") )
+            .settings.size("auto")  // Use max width for each cell (here, lukewarm's cell's width)
+            .print()
+            .wait()
     )]
 ];
